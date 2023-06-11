4632eaaf0d84878a0ca419ed3a6d333bd6596d0b
Aiswarya-Prasad <aiswarya.prasad1998@gmail.com>
2023-06-11 20:49:09 +0200

    Lots of updates to visualization scripts. Work in progress.

--- scripts/calc_genome_sizes.sh
+++ scripts/calc_genome_sizes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+bins=$1
+outfile=$2
 cd /scratch/aprasad/211018_Medgenome_india_samples
 echo "ID, length" > Figures/Genome_sizes.csv
-for file in 07_AnnotationAndPhylogenies/00_genomes/*.fa; do echo "$(echo ${file##*mes/} | sed -e 's/\.fa//'),$(cat $file | grep -v ">" | wc -c)" >> Figures/Genome_sizes.csv; done
+for file in ${bins}/*.fa; do echo "$(echo ${file##*mes/} | sed -e 's/\.fa//'),$(cat $file | grep -v ">" | wc -c)" >> ${outfile}; done
