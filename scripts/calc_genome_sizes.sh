998a14688ea87b5ec4e556e519a823d2302e4450
Aiswarya-Prasad <aiswarya.prasad1998@gmail.com>
2023-09-04 18:18:54 +0200

    updated R scripts for vis

--- scripts/calc_genome_sizes.sh
+++ scripts/calc_genome_sizes.sh
@@ -1,34 +1,6 @@
-#! /usr/bin/perl -w
-use strict;
-use Bio::Seq;
-use Bio::SeqIO;
-use Getopt::Long;
-use Pod::Usage;
-pod2usage(-verbose => 2) if ((@ARGV==0)&&(-t STDIN));
-
-=head1 SYNOPSIS
-
-perl calc_assembly_length.pl AmAi01_1_parsed.fasta
-
-=head1 DESCRIPTION
-
-This script is used to calculate the total sequence length of a multi-fasta file, it will print the length (bp) to stdout. The script was used to calculate assembly lengths for rarefied filtered metagenomic assemblies.
-
-=cut
-
-GetOptions(
-    'help' => sub { pod2usage( -exitstatus => 0, -verbose => 2 ) },
-);
-
-my $input_file = shift;
-my $seq_io = Bio::SeqIO->new(
-                      -file => $input_file, 
-                       -format => 'fasta',
-                       );
-my $tot_length=0;
-while (my $seq_obj = $seq_io->next_seq()) {
-    my $seq_length = $seq_obj->length;
-    $tot_length += $seq_length;
-}
-
-print $tot_length,"\n";
+#!/bin/bash
+bins=$1
+outfile=$2
+ext=$3
+echo "ID, length" > ${outfile}
+for file in ${bins}/*.${ext}; do filename=$(basename $file) ; echo "$(echo ${filename##*mes/} | sed -e "s/\.${ext}//"),$(cat $file | grep -v ">" | wc -c)" >> ${outfile}; done
